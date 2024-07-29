8c48c7b93d350636afe1b0bf0408184cfbb88ef6
Andriantomanga <nabil.andriantomanga@gmail.com>
2024-07-30 01:11:32 +0200

    E.G: petites refacto

--- src/main/java/com/endrikagallery/artworks/andriantomanga/CircleConstellationArtwork.java
+++ src/main/java/com/endrikagallery/artworks/andriantomanga/CircleConstellationArtwork.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2024 Nabil Andriantomanga
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.endrikagallery.artworks.andriantomanga;
 
 import com.endrikagallery.AbstractArtwork;
@@ -10,10 +32,15 @@ import java.awt.Graphics2D;
 import java.awt.geom.GeneralPath;
 import java.util.Random;
 
+/**
+ * @author Nabil Andriantomanga
+ * @version 1.0
+ * @since 2024
+ */
 public class CircleConstellationArtwork extends AbstractArtwork {
 
-    public static final int DELTA_X = 90;
-    public static final int DELTA_Y = 90;
+    private static final int DELTA_X = 90;
+    private static final int DELTA_Y = 90;
 
     public CircleConstellationArtwork(ArtworkInfos artworkInfos, ArtistInfos artistInfos) {
         super(artworkInfos, artistInfos);
@@ -21,13 +48,10 @@ public class CircleConstellationArtwork extends AbstractArtwork {
 
     @Override
     public void expresses(Graphics graphics) {
-//        int[] xValues = {55, 67, 109, 73, 83, 55, 27, 37, 1, 43};
-//        int[] yValues = {0, 36, 36, 54, 96, 72, 96, 54, 36, 36};
-
         int[] xValues = {110, 134, 218, 146, 166, 110, 54, 74, 2, 86};
         int[] yValues = {0, 72, 72, 108, 192, 144, 192, 108, 72, 72};
 
-        Graphics2D graphics2D = (Graphics2D) graphics.create();
+        var graphics2D = (Graphics2D) graphics.create();
         var path = new GeneralPath();
 
         path.moveTo(DELTA_X + xValues[0], DELTA_Y + yValues[0]);
@@ -38,7 +62,7 @@ public class CircleConstellationArtwork extends AbstractArtwork {
 
         path.closePath();
         graphics2D.translate(DELTA_X + 150, DELTA_Y + 150);
-        Random rNumbers = new Random();
+        var rNumbers = new Random();
         for (int i = 1; i <= 20; i++) {
             graphics2D.rotate(Math.PI / 10.0, DELTA_X, DELTA_Y);
             graphics2D.setColor(new Color(rNumbers.nextInt(256), rNumbers.nextInt(256), rNumbers.nextInt(256)));
--- src/main/java/com/endrikagallery/artworks/andriantomanga/SpiralArtwork.java
+++ src/main/java/com/endrikagallery/artworks/andriantomanga/SpiralArtwork.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2024 Nabil Andriantomanga
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.endrikagallery.artworks.andriantomanga;
 
 import com.endrikagallery.AbstractArtwork;
@@ -9,6 +31,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
+/**
+ * @author Nabil Andriantomanga
+ * @version 1.0
+ * @since 2024
+ */
 public class SpiralArtwork extends AbstractArtwork {
     public SpiralArtwork(ArtworkInfos artworkInfos, ArtistInfos artistInfos) {
         super(artworkInfos, artistInfos);
@@ -17,19 +44,19 @@ public class SpiralArtwork extends AbstractArtwork {
     @Override
     public void expresses(Graphics graphics) {
         Graphics2D g2d = (Graphics2D) graphics;
-        final int centerX = 400;
-        final int centerY = 300;
-        final int radius = 200;
-        final int numTurns = 10;
-        final int numPoints = 100;
+        var centerX = 400;
+        var centerY = 300;
+        var radius = 200;
+        var numTurns = 10;
+        var numPoints = 100;
 
         g2d.setStroke(new java.awt.BasicStroke(2));
 
         for (int i = 0; i < numPoints; i++) {
-            double angle = 2 * Math.PI * i / numPoints * numTurns;
-            double r = radius * (i / (double) numPoints);
-            int x = (int) (centerX + r * Math.cos(angle));
-            int y = (int) (centerY + r * Math.sin(angle));
+            var angle = 2 * Math.PI * i / numPoints * numTurns;
+            var r = radius * (i / (double) numPoints);
+            var x = (int) (centerX + r * Math.cos(angle));
+            var y = (int) (centerY + r * Math.sin(angle));
 
             g2d.setColor(new Color(
                     (int) (Math.random() * 256),
